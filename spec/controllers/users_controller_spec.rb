9439610cfec421858b66f2373a6f7b8a92ab2c87
Dusan Milic <milicdusan89@gmail.com>
2016-08-02 17:32:07 +0200

    Added a test to users controller

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,15 +2,21 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe 'GET #show' do
-    user = FactoryGirl.create(:user)
+    user = FactoryGirl.create(:user_with_posts)
     before do
       post :show, user_id: user.id
     end
+
     it 'renders show template' do
       expect(response).to render_template('show')
     end
+
     it 'assigns user to template' do
       expect(assigns(:user)).to match(user)
+    end
+    
+    it 'assigns posts to template' do
+      expect(assigns(:posts)).to match(user.posts)
     end
   end
 end
