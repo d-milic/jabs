8f3f97c71287ed881dd5dfe1b77e55a54d562548
Dusan Milic <milicdusan89@gmail.com>
2016-09-12 17:12:33 +0200

    Made changes because of different routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # Controller that handles user CRUD operations
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:user_id])
+    @user = User.find_by_username(params[:username])
     @posts = @user.posts
   end
 end
